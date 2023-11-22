e509e3a3456b90cbfe6d0c15184483ee09395993
juan.sotocastro <juan.sotocastro@ucr.ac.cr>
2023-11-22 02:24:34 -0600

    Fase3 terminada

--- tareasProgramadas/target7722/phase3.d
+++ tareasProgramadas/target7722/phase3.d
@@ -6,6 +6,4 @@ Disassembly of section .text:
 
 0000000000000000 <.text>:
    0:	48 c7 c7 c0 1c 68 05 	mov    $0x5681cc0,%rdi
-   7:	48 c7 c4 a8 1c 68 05 	mov    $0x5681ca8,%rsp
-   e:	68 f9 1d 40 00       	push   $0x401df9
-  13:	c3                   	ret
+   7:	c3                   	ret
--- tareasProgramadas/target7722/phase3.s
+++ tareasProgramadas/target7722/phase3.s
@@ -1,4 +1,2 @@
-mov $0x5681CC0, %rdi 
-mov $0x5681CA8, %rsp
-push $0x401df9
+mov $0x55681cc0,%rdi
 ret
